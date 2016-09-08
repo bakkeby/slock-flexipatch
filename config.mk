cd3c546c37d91e24b8c55bab88bfcb920d8ea895
Quentin Rameau <quinq@fifth.space>
2016-09-08 19:03:12 +0200

    config.mk: be more explicative about FLAGS
    Group each *FLAG with its description and add a NetBSD specific.

--- config.mk
+++ config.mk
@@ -20,10 +20,12 @@ CFLAGS = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
 LDFLAGS = -s ${LIBS}
 COMPATSRC = explicit_bzero.c
 
-# On *BSD remove -DHAVE_SHADOW_H from CPPFLAGS
 # On OpenBSD and Darwin remove -lcrypt from LIBS
 #LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 -lXext -lXrandr
-#CPPFLAGS = -DVERSION=\"${VERSION}\" -D_BSD_SOURCE
+# On *BSD remove -DHAVE_SHADOW_H from CPPFLAGS
+# On NetBSD add -D_NETBSD_SOURCE to CPPFLAGS
+#CPPFLAGS = -DVERSION=\"${VERSION}\" -D_BSD_SOURCE -D_NETBSD_SOURCE
+# On OpenBSD set COMPATSRC to empty
 #COMPATSRC =
 
 # compiler and linker
