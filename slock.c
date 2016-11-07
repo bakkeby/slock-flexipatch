7a604ec1fae1eda36f210a28a2cf466583a9cb87
Bob Uhl <buhl@zvelo.com>
2016-11-07 10:15:20 -0700

    Fix resize with multiple monitors and portrait mode
    When connecting/disconnecting a portrait monitor, the
    XRRScreenChangeNotifyEvent height & width are reversed due to the XRandR
    rotation; detect this and DTRT.

--- slock.c
+++ slock.c
@@ -201,8 +201,13 @@ readpw(Display *dpy, struct xrandr *rr, struct lock **locks, int nscreens,
 			rre = (XRRScreenChangeNotifyEvent*)&ev;
 			for (screen = 0; screen < nscreens; screen++) {
 				if (locks[screen]->win == rre->window) {
-					XResizeWindow(dpy, locks[screen]->win,
-					              rre->width, rre->height);
+					if (rre->rotation == RR_Rotate_90 ||
+					    rre->rotation == RR_Rotate_270)
+						XResizeWindow(dpy, locks[screen]->win,
+						              rre->height, rre->width);
+					else
+						XResizeWindow(dpy, locks[screen]->win,
+						              rre->width, rre->height);
 					XClearWindow(dpy, locks[screen]->win);
 				}
 			}
