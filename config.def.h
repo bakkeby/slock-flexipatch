ae96836f90c3652fd0907e0632ddd4591d5716fa
Markus Teich <markus.teich@stusta.mhn.de>
2016-11-20 00:29:41 +0100

    clarify colors in config.def.h

--- config.def.h
+++ config.def.h
@@ -3,10 +3,10 @@ static const char *user  = "nobody";
 static const char *group = "nogroup";
 
 static const char *colorname[NUMCOLS] = {
-	"black",     /* after initialization */
-	"#005577",   /* during input */
-	"#CC3333",   /* wrong password */
+	[INIT] =   "black",     /* after initialization */
+	[INPUT] =  "#005577",   /* during input */
+	[FAILED] = "#CC3333",   /* wrong password */
 };
 
-/* treat a cleared input like a wrong password */
+/* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
