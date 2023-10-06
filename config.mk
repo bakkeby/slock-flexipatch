66fc3b5ae01b2daadf63fcd3c48cd5f1424cc739
bakkeby <bakkeby@gmail.com>
2023-10-06 12:36:54 +0200

    config.mk: no need to set CC
    ref.
    https://git.suckless.org/slock/commit/2fec14c567411b939e717c380d8b4460d462b146.html

--- config.mk
+++ config.mk
@@ -43,6 +43,3 @@ COMPATSRC = explicit_bzero.c
 #CPPFLAGS = -DVERSION=\"${VERSION}\" -D_BSD_SOURCE -D_NETBSD_SOURCE
 # On OpenBSD set COMPATSRC to empty
 #COMPATSRC =
-
-# compiler and linker
-CC = cc
